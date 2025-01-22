f696f68bee164d66b9df56345b03c08fed547935
Hariom9076 <hariomchauraisya@gmail.com>
2025-01-22 11:25:19 +0000

    operation done

--- Basics/conversionoperators.js
+++ Basics/conversionoperators.js
@@ -62,3 +62,40 @@ let somenumber =33
 let stringnumber = String(somenumber)
 console.log(stringnumber) // 33
 console.log(typeof stringnumber) // string
+
+
+
+//---operations
+
+let value =3;
+let negvalue = -value;
+console.log(negvalue);//-3 ans
+
+console.log(2+2); //4
+console.log(2-2); //0
+console.log(2*2); //4
+console.log(2/2); //1
+console.log(2**3); //8   yeh 2 ki power 3 bana deta h
+console.log(2%3); //2
+
+
+let str1 = "Hello"
+let str2 = " Hariom"
+
+let str3 = str1 + str2
+console.log(str3);//Hello Hariom
+
+console.log("1" + 2); //12
+console.log(1 + "2");//12
+console.log("1" + 2 + 2); //122
+/*
+if string come first then all treat as string and comes last then first will conversion nd then 
+string operation
+*/
+console.log(1 + 2 + "2"); //32  here string come last so first conversion
+
+console.log(true); //true
+console.log(+true); //1
+console.log(+""); //0
+
+
