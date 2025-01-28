e4bad97cbec0561295134b33390ea5e06f237831
Hariom9076 <hariomchauraisya@gmail.com>
2025-01-28 07:40:32 +0000

    done

--- Basics2/array2.js
+++ Basics2/array2.js
@@ -36,3 +36,6 @@ let score2 = 200
 let score3 = 300
 
 console.log(Array.of(score1,score2,score3)); //[100,200,300]
+
+let score14 =100 ,score15 =14 ,score16 = 17
+console.log(Array.of(score14,score15,score16)); // [100,14,17]
